6ca0753449aec21a7d82219e064beccce6656e55
aayushdc <89606048+aayushdc@users.noreply.github.com>
2022-11-22 20:24:53 +0545

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatefulWidget {
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My 1st App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: myAppScreen(),
+    );
+  }
+}
+
+class myAppScreen extends StatefulWidget {
+  @override
+  State<myAppScreen> createState() => _myAppScreenState();
+}
+
+class _myAppScreenState extends State<myAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('1st App')),
+      body: Center(child: Container(child: Text('Welcome to my 1st App!')),),
+    );
+  }
+}
